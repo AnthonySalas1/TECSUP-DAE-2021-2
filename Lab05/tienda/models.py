2feb522b91828d75a8d1f78e0948433fd582b796
Anthony Salas <anthony.salas@tecsup.edu.pe>
2021-09-22 13:16:09 -0500

    Lab05

---
+++ Lab05/tienda/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ManyToManyField
+
+# Create your models here.
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+    dni = models.IntegerField(max_length=8)
+    telefono = models.IntegerField(max_length=12)
+    email = models.EmailField(max_length=50)
+    pub_date = models.DateTimeField('date published')
+
+
+    def __str__(self):
+    
+        return self.nombres + " " + self.apellidos 
+
+        
+
