64203a7f30cec1a399c4c3006c9cbd1ea66c92ac
Anthony Salas <anthony.salas@tecsup.edu.pe>
2021-10-03 20:34:07 -0500

    Lab06 Terminado

--- Lab06/tienda/urls.py
+++ Lab06/tienda/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
 
+
 from . import views
 
 app_name = 'tienda'
 
 urlpatterns = [
     path('', views.index, name='index'),
-   
+    path('producto', views.producto, name='producto')
+    
 ]
